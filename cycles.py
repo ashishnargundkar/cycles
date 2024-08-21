087e54dcec25c026d60fda6c793b3f8bb427c736
Ashish Nargundkar <apnargundkar@gmail.com>
2024-08-20 22:34:36 -0400

    Enhanced cycles modelling with sense_of_duality and sense_of_love_and_oneness

--- cycles.py
+++ cycles.py
@@ -2,16 +2,17 @@ import random
 import logging
 
 
-logging.basicConfig(level=logging.INFO,
+logging.basicConfig(level=logging.DEBUG,
                     format="%(message)s")
 
 
 MAX_SENSE_OF_DUALITY = 100
+MAX_SENSE_OF_ONENESS_AND_LOVE = MAX_SENSE_OF_DUALITY
 
 # Only when you have built a sufficient sense of oneness and dissolved your
 # duality will you start your monotonically increasing progress towards God,
 # Truth, Self
-ONENESS_THRESHOLD_FOR_GODS_LONGING = 20
+ONENESS_THRESHOLD_FOR_GODS_LONGING = 30
 
 # Oneness increases every lifetime after crossing a certain threshold. Prior to
 # that, for each lifetime, it increases subject to a probability which is a
@@ -24,24 +25,29 @@ KARMA_INCREMENT_PER_LIFETIME_ESTIMATE = 5
 
 # An estimate of how much karma you burn when you have a sufficiently high
 # sense of oneness. Note that karma is easier to accrue than eliminate.
-KARMA_DECREMENT_PER_LIFETIME_ESTIMATE = 2
+KARMA_DECREMENT_PER_MATERIAL_LIFETIME_ESTIMATE = 2
+
+# You can be on the path to God despite a high karma. Things will fall into
+# place, don't worry!.
+# KARMA_THRESHOLD_FOR_GODS_LONGING = 300
 
 # You cannot attain enlightenment until your karma is sufficiently lowered
-KARMA_THRESHOLD_FOR_ENLIGHTENMENT = 5
+KARMA_THRESHOLD_AFTER_ENLIGHTENMENT = 5
 
 # Number of lifetimes you need to carry out God's work AFTER you attain the
-# knowledge of the self. You will attain moksha when this counter goes to
-# 0.
+# knowledge of the Self. You will attain moksha when this counter goes to
+# 0. Please see comments in live() to understand what is God's work.
 LIFETIMES_TO_COMPLETE_GODS_WORK = 5
 
 
 # You don't have this, to begin with, that's why the whole cycle of births and
 # deaths over and over again!
 have_knowledge_of_self = False
+on_gods_path = False
 
 # Initialised with your evolutionary karma, assumed to be constant for
 # everyone
-karma = 50
+karma = 100
 # Should we model it this way instead?
 # karma = random.randrange(100 + 1)  # initialised with your evolutionary karma
 
@@ -73,51 +79,64 @@ def be_born():
     total_lives += 1
     logging.debug("tl: {} | lives_used_for_gods_work: {} | lives_wasted: {}"
                   " | lives_for_lowering_karma_after_adopting_gods_path: {}"
-                  " | karma: {}"
+                  " | duality: {} | love_and_oneness: {} | karma: {:.2f}"
                   .format(total_lives,
                           lives_used_for_gods_work,
                           lives_wasted,
                           lives_for_lowering_karma_after_adopting_gods_path,
+                          sense_of_duality,
+                          sense_of_love_and_oneness,
                           karma))
 
 
 def live():
+    global on_gods_path
     global lives_for_lowering_karma_after_adopting_gods_path
 
     if not have_knowledge_of_self:
         if sense_of_love_and_oneness < ONENESS_THRESHOLD_FOR_GODS_LONGING:
+           # or karma > KARMA_THRESHOLD_FOR_GODS_LONGING:
             # Awaken consciousness by a bit, subject to probability of
             # conducive events occurring in your lifetime.
             # Note that this doesn't depend on karma. You can still be on the
             # path to God despite a high accumulation of karma.
             consider_physical_world_as_ultimate_reality()
         else:
+            if not on_gods_path:
+                on_gods_path = True
+                logging.info("Commenced on the path to God at lifetime"
+                             " {}".format(total_lives))
+
+            lives_for_lowering_karma_after_adopting_gods_path += 1
+
             # This is where you adopt God's path and keep reducing karma and
             # duality in order to attain enlightenment
             # Remember, the more karma you accrue, the more lifetimes it takes
             # to eliminate it sufficiently
-            lives_for_lowering_karma_after_adopting_gods_path += 1
-            inch_towards_truth()
+            inch_towards_truth(rapidly=True)
 
         # When you have absolutely obliterated any sense of duality, you attain
-        # knowledge of the self
-        if not sense_of_duality and karma <= KARMA_THRESHOLD_FOR_ENLIGHTENMENT:
+        # knowledge of the Self
+        if not sense_of_duality:
             attain_knowledge_of_self()
-        # else you'll have to take more birth(s)!
-    # else you're carrying out God's work
+        # else: you'll have to take more birth(s)!
+    # else: you're spending the lifetime carrying out God's work, that of
+    # spreading awareness of the Self to the masses and getting as many as
+    # possible on the path
 
 
-def inch_towards_truth():
+def inch_towards_truth(rapidly=False):
     global lives_for_lowering_karma_after_adopting_gods_path
 
     # Awaken consciousness bit by bit without risking wasting an entire
     # lifetime
-    awaken_oneness_and_shatter_duality()
+    awaken_oneness_and_shatter_duality(rapidly)
 
-    if karma > KARMA_THRESHOLD_FOR_ENLIGHTENMENT:
-        # You are consciously eliminating your karma
-        alleviate_karma()
-    # else you are consciously holding on to a minimum amount of karma to keep
+    if karma > KARMA_THRESHOLD_AFTER_ENLIGHTENMENT:
+        # You are consciously/unconsciously eliminating your karma depending
+        # upon whether you are on a clear path to enlightenment
+        alleviate_karma(rapidly)
+    # else: you are consciously holding on to a minimum amount of karma to keep
     # possession of your body for carrying out God's work
 
 
@@ -133,26 +152,69 @@ def consider_physical_world_as_ultimate_reality():
     entirely_caught_up_in_maya = random.randrange(6)
     if not entirely_caught_up_in_maya:
         logging.debug("Got lucky")
+
+        # You don't have Self realisation yet. By default, inching towards the
+        # truth happens without Self realisation (i.e. the
+        # "have_knowledge_of_self" parameter is False).
+        # You're not on a clear path to God yet. Hence, your karma will reduce
+        # only a tiny bit. Also oneness/duality will be affected in small
+        # amounts. You have no control yet over the "rapidly" parameter when
+        # inching towards the Truth.
         inch_towards_truth()
     else:
         aggravate_karma()
         lives_wasted += 1
 
 
-# Happens bit by bit
-def awaken_oneness_and_shatter_duality():
+def awaken_oneness_and_shatter_duality(rapidly=False):
     global sense_of_duality
     global sense_of_love_and_oneness
 
     if sense_of_duality:
-        sense_of_love_and_oneness += ONENESS_INCREMENT_PER_LIFETIME_ESTIMATE
-        sense_of_duality -= ONENESS_INCREMENT_PER_LIFETIME_ESTIMATE
+        if rapidly:
+            sense_of_love_and_oneness \
+                    += (2 * ONENESS_INCREMENT_PER_LIFETIME_ESTIMATE)
+            # To keep the numerical calculations correct (how silly to relegate
+            # creation to a mathematical model!), we fix an upper bound
+            # Remember again that this upper bound is the same as the upper
+            # bound on the sense of duality as both these quantities are
+            # duals of each other.
+            if sense_of_love_and_oneness > MAX_SENSE_OF_ONENESS_AND_LOVE:
+                sense_of_love_and_oneness = MAX_SENSE_OF_ONENESS_AND_LOVE
+
+            sense_of_duality -= (2 * ONENESS_INCREMENT_PER_LIFETIME_ESTIMATE)
+
+            # To keep the numerical calculations correct (how silly to relegate
+            # creation to a mathematical model!), we fix a lower bound
+            if sense_of_duality < 0:
+                sense_of_duality = 0
+        else:
+            sense_of_love_and_oneness \
+                += ONENESS_INCREMENT_PER_LIFETIME_ESTIMATE
+            sense_of_duality -= ONENESS_INCREMENT_PER_LIFETIME_ESTIMATE
 
 
-def alleviate_karma():
+def alleviate_karma(exponentially=False):
     global karma
 
-    karma -= KARMA_DECREMENT_PER_LIFETIME_ESTIMATE
+    if exponentially:
+        # When you are on a clear path to God (i.e. realisation of Self),
+        # you are in a hurry to burn your karma. You make every effort to
+        # get rid of you karma as fast as you can. With every lifetime, you
+        # get better and better, and faster and faster at burning karma.
+        karma_reduction_coeff = 2 * (1 + (sense_of_love_and_oneness / 100))
+        karma -= (karma_reduction_coeff
+                  * 5)
+
+        if karma < KARMA_THRESHOLD_AFTER_ENLIGHTENMENT:
+            # We are not letting you go beyond the minimum karma lest you
+            # accidentally slip out of your body without attaining knowledge of
+            # the Self! The physical equivalent of this is to place metal on
+            # strategic parts of the body to prevent its accidental
+            # renunciation.
+            karma = KARMA_THRESHOLD_AFTER_ENLIGHTENMENT
+    else:
+        karma -= KARMA_DECREMENT_PER_MATERIAL_LIFETIME_ESTIMATE
 
 
 def aggravate_karma():
@@ -170,12 +232,25 @@ def give_up_body(for_liberation=False, consciously=False):
 
 
 def attain_knowledge_of_self():
+    global karma
+    global total_lives
     global have_knowledge_of_self
 
     have_knowledge_of_self = True
+
+    # Once you become a master of the Self, you can manipulate karma like a
+    # child masterfully plays with its toys. You are immediately in a
+    # position to shed excess karma just as easily as you would shed clothes.
+    # However, because you still have to carry out God's work in the physical
+    # realm, you still need to hold on to some bit of karma - which is the
+    # cement between the Self and the body that the creator gave you.
+    karma = KARMA_THRESHOLD_AFTER_ENLIGHTENMENT
+
+    logging.info("Attained knowledge of the Self at lifetime"
+                 " {}".format(total_lives))
     logging.info("Wasted {} lives in realising the Truth".format(lives_wasted))
     logging.info("Took {} lives to sufficiently get rid of"
-                 " karma for realising the self".format(
+                 " karma for realising the Self".format(
                      lives_for_lowering_karma_after_adopting_gods_path))
 
 
@@ -191,7 +266,7 @@ while karma > 0:
     live()
 
     if not have_knowledge_of_self:
-        # When you don't have knowledge of the self, you have no control over
+        # When you don't have knowledge of the Self, you have no control over
         # the "consciously" parameter when giving up your body. Nature will
         # forcefully take it away from you. Similarly, you have no control over
         # the "for_liberation" parameter at this stage.
